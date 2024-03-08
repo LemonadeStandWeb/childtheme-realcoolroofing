de1b3cdd86638a53ab3b5ea590b6e139974b8da8
M. Scott O'Connor <michaeloconnor20@outlook.com>
2024-03-08 11:46:19 -0700

    Reviews - feature complete

--- ls-shortcodes/ls-shortcode-reviews.php
+++ ls-shortcodes/ls-shortcode-reviews.php
@@ -3,52 +3,48 @@
 function ls_shortcode_reviews($atts){
     ob_start();
 
-    $shorties ='';
+    // Define query arguments
+    $args = array(
+        'post_type' => 'reviews',
+        'posts_per_page'=> 3,
+    );
 
-    $shorties .= '[row style="small" col_bg="rgb(255,255,255)" v_align="equal" h_align="center" padding="40px 40px 40px 40px" depth="5"] 
-    
-    [col span="4" span__sm="12" span__md="10" align="left" animate="fadeInUp"]
+    // Run the query
+    $query = new WP_Query( $args );
 
-    [featured_box img="262" icon_color="rgb(60, 156, 214)"]
-    
-    <p class="mb-0">Review goes here</p>
-    
-    [/featured_box]
-    [gap height="15px"]
-    
-    <h6>Name hERE</h6>
-    
-    [/col] 
+    if( $query->have_posts() ) {
 
-    [col span="4" span__sm="12" span__md="10" align="left" animate="fadeInUp"]
+        $shorties ='';
+        $shorties .= '[row style="small" col_bg="rgb(255,255,255)" v_align="equal" h_align="center" padding="40px 40px 40px 40px" depth="5"]';
+        
+        while( $query->have_posts() ) {
+            $query->the_post();
 
-    [featured_box img="262" icon_color="rgb(60, 156, 214)"]
-    
-    <p class="mb-0">Review goes here</p>
-    
-    [/featured_box]
-    [gap height="15px"]
-    
-    <h6>Name hERE</h6>
-    
-    [/col] 
+            $ls_review_text = get_field( 'ls_review_text' );
+            $ls_name = get_field('ls_name');
 
-    [col span="4" span__sm="12" span__md="10" align="left" animate="fadeInUp"]
+            if( $ls_review_text && $ls_name ) {
 
-    [featured_box img="262" icon_color="rgb(60, 156, 214)"]
-    
-    <p class="mb-0">Review goes here</p>
-    
-    [/featured_box]
-    [gap height="15px"]
-    
-    <h6>Name hERE</h6>
-    
-    [/col] 
-    
-    [/row]';
+                $shorties .= '[col span="4" span__sm="12" span__md="10" align="left" animate="fadeInUp"]
 
-    echo do_shortcode($shorties);
+                [featured_box img="262" icon_color="rgb(60, 156, 214)"]
+                
+                <p class="mb-0"> ' . $ls_review_text . ' </p>
+                
+                [/featured_box]
+                [gap height="15px"]
+                
+                <h6>' . $ls_name . '</h6>
+                
+                [/col]';
+                
+            }
+        }
+
+        $shorties .= '[/row]';
+
+        echo do_shortcode($shorties);
+    }
 
     return ob_get_clean();
 }
